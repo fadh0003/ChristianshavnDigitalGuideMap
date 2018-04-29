175480daae2c99cb690aa756a92cea9968f600ad
fadh0003 <fadh0003@stud.kea.dk>
2018-04-29 13:50:45 +0200

    app update

--- wwwroot/JS/GoogleMap.js
+++ wwwroot/JS/GoogleMap.js
@@ -1,20 +1,5 @@
-var Places = {
-    Locations: {
-        CafeHalvandet:{lat: 55.695443, lng: 12.609745},
-        LaBanchina:{lat: 55.689343, lng: 12.610985},
-        Margretheholmhavn:{lat: 55.689658, lng: 12.619308},
-        Sofiebadet:{lat: 55.670969, lng: 12.590944}
-    },
-    LocationsInfo:{
-        CafeHalvandetInfo:"<h2>Café Halvandet</h2> <h3>Address: 325 Refshalevej København K, 1432 København</h3> <div><a href='https://www.google.dk/maps/dir//Cafe+Halvandet'>Click to get Directions to Café Halvandet</a></div>",
-        LaBanchinaInfo: "<h2>La Banchina</h2> <h3>Address: Refshalevej 141A, 1432 København</h3> <div><a href='https://www.google.dk/maps/dir//La+Banchina'>Click to get Directions to La Banchina</a></div>",
-        MargretheholmhavnInfo: "<h2>Lynetten, Margretheholms havn</h2> <h3>Address: Refshalevej 200, 1432 København</h3> <div><a href='https://www.google.dk/maps/dir//lynetten'>Click to get Directions to Lynetten, Margretheholms havn</a></div>",
-        SofiebadetInfo: "<h2>Sofiebadet</h2> <h3>Address: Sofiegade 15, 1418 København</h3> <div><a href='https://www.google.dk/maps/dir//Sofiebadet'>Click to get Directions to Sofiebadet</a></div>"
-    }
-}
-
-
 function initMap() {
+    
     var options = {
         center: {lat: 55.6761, lng:12.5683},
         zoom: 10,
@@ -22,6 +7,22 @@ function initMap() {
         maxZoom: 18,
         fullscreenControl: false
     }
+
+    var Places = {
+        Locations: {
+            CafeHalvandet:{lat: 55.695443, lng: 12.609745},
+            LaBanchina:{lat: 55.689343, lng: 12.610985},
+            Margretheholmhavn:{lat: 55.689658, lng: 12.619308},
+            Sofiebadet:{lat: 55.670969, lng: 12.590944}
+        },
+        LocationsInfo:{
+            CafeHalvandetInfo:"<h2>Café Halvandet</h2> <h3>Address: 325 Refshalevej København K, 1432 København</h3> <div><a href='https://www.google.dk/maps/dir//Cafe+Halvandet'>Click to get Directions to Café Halvandet</a></div>",
+            LaBanchinaInfo: "<h2>La Banchina</h2> <h3>Address: Refshalevej 141A, 1432 København</h3> <div><a href='https://www.google.dk/maps/dir//La+Banchina'>Click to get Directions to La Banchina</a></div>",
+            MargretheholmhavnInfo: "<h2>Lynetten, Margretheholms havn</h2> <h3>Address: Refshalevej 200, 1432 København</h3> <div><a href='https://www.google.dk/maps/dir//lynetten'>Click to get Directions to Lynetten, Margretheholms havn</a></div>",
+            SofiebadetInfo: "<h2>Sofiebadet</h2> <h3>Address: Sofiegade 15, 1418 København</h3> <div><a href='https://www.google.dk/maps/dir//Sofiebadet'>Click to get Directions to Sofiebadet</a></div>"
+        }
+    }
+
     var map = new google.maps.Map(document.getElementById('map'), options);
     
     var markerCluster = new MarkerClusterer(map, [],
